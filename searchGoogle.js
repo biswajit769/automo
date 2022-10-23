aa4eda594a637e875e2e170f0f30212d6c957fac
biswajit769 <biswajit769@gmail.com>
2022-10-23 18:15:01 +0530

    changes

--- searchGoogle.js
+++ searchGoogle.js
@@ -36,7 +36,7 @@ const searchGoogle = async (searchQuery) => {
     })
     .catch((err) => console.log("error loading url", err));
     //console.timeEnd("goto");
-    await page.waitFor(1000)
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
     //wait for one of the div classes to load
     await page.waitForSelector('#kt_login > div > div > kt-login > div > div');
